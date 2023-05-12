2a9d9df6f8d2062b3cbbfd8d73ac57c6b3851417
AbidovEl <elyor132000@mail.ru>
2023-05-12 23:38:29 +0500

    initial commit

--- src/store.js
+++ src/store.js
@@ -44,7 +44,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.getNewCode(this.state.list), title: 'Новая запись', count: 0}]
     })
   };
 
@@ -69,10 +69,41 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          // Проверяет выделен ли объект чтобы вести счет и отменить выделение предыдущего объекта
+          if(item.selected){
+            this.state.list.map(i => {
+              if(i.code !== code && i.selected){
+                i.selected = false
+              }
+            })
+            item.count++;
+          }
         }
         return item;
       })
     })
+  }
+  // Для уникальности кода
+  getNewCode(list){
+    let max = 0
+    for(let item of list){
+        if(item.code > max){
+            max = item.code
+        }
+    }
+    return max + 1
+  }
+
+  // Для правильного написания раз или раза
+  getRazOrRaza(num){
+    if (num % 10 === 1 && num % 100 !== 11) {
+      return num + ' раз';
+    } else if ((num % 10 === 2 || num % 10 === 3 || num % 10 === 4) && 
+              !(num % 100 >= 12 && num % 100 <= 14)) {
+      return num + ' раза';
+    } else {
+      return num + ' раз';
+    }
   }
 }
 
